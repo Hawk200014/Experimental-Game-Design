50262a62fdafa3edf43528dbb9e577c5226af0c1
Hawk200014 <72735051+Hawk200014@users.noreply.github.com>
2021-11-05 15:33:43 +0100

    Trying to get the mousehit on the player

--- Experimental Game Design Projekt/Assets/Scipts/PlayerMovementScript.cs
+++ Experimental Game Design Projekt/Assets/Scipts/PlayerMovementScript.cs
@@ -7,6 +7,7 @@ public class PlayerMovementScript : MonoBehaviour
     private Rigidbody2D rigidbody2D;
     [SerializeField] float movespeed = 100f;
     [SerializeField] float veloX;
+    [SerializeField] Vector2 mousePos;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,23 @@ public class PlayerMovementScript : MonoBehaviour
     void Update()
     {
         this.veloX = rigidbody2D.velocity.x;
+        this.mousePos = Input.mousePosition;
 
+        if(Input.GetMouseButtonDown(0)){
+            print("Maustaste wurde gedrückt");
+            RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, Vector2.zero);
+            if(hit != null){
+                print("Object hit: " + hit.transform.name);
+            }
+            else{
+                print("Hit == null");
+            }
+            
+        }
+
+
+
+        /*
         if(Input.GetKey(KeyCode.D)){
             rigidbody2D.AddForce(new Vector2(movespeed*Time.deltaTime,0));
         }
@@ -30,5 +47,6 @@ public class PlayerMovementScript : MonoBehaviour
             else if(rigidbody2D.velocity.x > 0)
                 rigidbody2D.AddForce(new Vector2(- rigidbody2D.velocity.x * 0.5f, 0));
         }
+        */
     }
 }
